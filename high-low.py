0a8f3540c73d6cebf62e2877189f6c1d3c9d8adb
Shatakshi-Shiravale <shiravaleshatakshi@gmail.com>
2025-10-11 22:14:08 +0530

    Add files via upload
    Guess the number while specifying a particular domain of your choice!

---
+++ high-low.py
@@ -0,0 +1,30 @@
+import random
+
+lower=int(input("enter lower bound"))
+upper=int(input("enter upper bound"))
+print("You have 5 guesses to guess the number")
+x=random.randint(lower,upper)
+print(x)
+guess=0
+if upper<lower:
+    print("Error, upper bound is smaller than lower bound")
+else:
+    while guess<5:
+        guess = guess + 1
+        x1 = int(input("enter your guess"))
+        if guess==5 and x1!=x:
+            print("Sorry! You couldn't guess the number.")
+            break
+        elif upper>=x1>x:
+            print("Guess lower")
+        elif lower<=x1<x:
+            print("Guess higher")
+        elif x1==x:
+            print("Congratulations, you guessed the number in",guess, "trial(s).")
+            break
+        else:
+            print("No out of range")
+
+
+
+
