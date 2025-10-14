d8dcffb4421bb5361004a500c4e34ce37b04ab74
Shatakshi Shiravale <shiravaleshatakshi@gmail.com>
2025-10-14 19:53:58 +0530

    palindrome_or_not.py

--- palindrome_or_not.py
+++ palindrome_or_not.py
@@ -1,7 +1,8 @@
 #program to check if palindrome or not
 
-num=int(input("Enter a number to check whether palindrome or not"))
+num=list(input("Enter a number to check whether palindrome or not"))
 if num==num[::-1]:
     print("palindrome")
 else:
     print("not palindrome")
+
